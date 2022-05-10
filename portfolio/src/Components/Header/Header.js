64ec31a2a2a4d65eb08ad63aa064aa63d96f8dc5
Yohann <yohann.delacroix@hotmail.fr>
2022-05-10 18:24:18 +0200

    Header style

--- portfolio/src/Components/Header/Header.js
+++ portfolio/src/Components/Header/Header.js
@@ -1,14 +1,16 @@
 import {Link} from 'react-router-dom';
+import './Header.css'
 
 function Header(){
   return(
-  <header>
-    <h1>Yohann Delacroix</h1>
-    <nav>
-      <ul>
-        <li><Link to="/Projects">Projects</Link></li>
-        <li><Link to="/About">About</Link></li>
-        <li><Link to="/Contact">Contact</Link></li>
+  <header className="main-header flex-header">
+    <h1 className="header-title flex-title">Yohann Delacroix</h1>
+
+    <nav className="nav-header flex-nav">
+      <ul className="ul-header">
+        <li><Link className="link-header"to="/Projects">Projects</Link></li>
+        <li><Link className="link-header" to="/About">About</Link></li>
+        <li><Link className="link-header" to="/Contact">Contact</Link></li>
       </ul>
     </nav>
   </header>)
