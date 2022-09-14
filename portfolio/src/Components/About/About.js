1faf198d1a7285f6f5d78b022c868ab3ad8e9520
Yohann <yohann.delacroix@hotmail.fr>
2022-09-14 12:15:38 +0200

    Update portfolio

--- portfolio/src/Components/About/About.js
+++ portfolio/src/Components/About/About.js
@@ -24,25 +24,27 @@ function About(){
       </p>
 
       <p>
-        One thing i consider really important is the user experience, to offer an understable
-        and intuitive design is a priority to me.
+        That's why i'm always focused on the User Experience, i love to implement the best and the more intuitive interfaces !
       </p>
+
 
       <h2>My experience</h2>
       <p>
       For the last three years, i've been focused on my IT bachelor degree that i completed on
-      April 2022, and i'm currently aiming to begin a profesionnal career in front-end development.
+      April 2022.
       </p>
+
       <p>
-      Through the web project we realized at school : Viva Musica,
-      i learned some basic knowledge in back-end and Webdesign.
+      When i was into the last bachelor's last year, i have implemented a musical searching engine called Viva musica.
       </p>
+      
       <p>
-      At the moment, i am working on a new project : "Red Light Distortion", a website for a music band, it will soon appears in my projects folder.
+      I have also worked for a music band on their website : Red Light Distortion <br />
+      I have designed the UI/UX and i am currently implementing the back-end stuff.
       </p>
 
       <div className="updated-date">
-        Updated the Friday 19, August, 2022
+        Updated the Wednesday 14, September, 2022
       </div>
       </div>
 
